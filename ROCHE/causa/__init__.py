04ea4a446baa1b27111c3c3ca584693041db4e28
Arann29 <andres.arostegui95@gmail.com>
2026-02-05 19:44:40 +0100

    fixed readme typo

--- ROCHE/causa/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
-# ROOT = '/'.join(PACKAGE_DIR.split('/')[:-1])
-# DATA_DIR = ROOT + '/data'
-
-PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))  # This points to the `causa` folder
-ROOT = os.path.join(PACKAGE_DIR, "..")  # Go up one level to `ROCHE`
-DATA_DIR = os.path.join(PACKAGE_DIR, "../../DATA")  # Point to `data` directory within `ROCHE`
